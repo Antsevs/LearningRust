2a7faca1c36f143f9559999565566d51a95edf56
Anthony Sevarino <sevarinoanthony@gmail.com>
2025-09-03 12:34:53 -0400

    <Learning>(<project>): New commit message template. <learning>(<fundamentals>): updated data_types to include info on boolean, float.
    This commit will mark the first where I introduce a new template for creating git commit messages.
    Also, data type fundamentals have been updated with new information regarding booleans and floats.

--- fundamentals/data_types/src/main.rs
+++ fundamentals/data_types/src/main.rs
@@ -1,7 +1,7 @@
 fn main(){
     //rust is statically typed (must know type of all vars at compile time)
     //the compiler can make this decision when its unsure, but we can define type by annotating
-    let guess: u32 = "ten".parse().expect("Not a number!");
+    let guess: u32 = "42".parse().expect("Not a number!");
     println!("{guess}");
 
     //Types include:
@@ -15,5 +15,10 @@ fn main(){
                     //Binary (0b1111_0000) = 11110000
                     //Byte, limited to u8 (b'A')        
                     //At compile time, integer overflow caught as error, but in release, it doesn't and simply overflows.
-                    //libraries to handle the above are wrapping_*, checked_*, overflowing_*, and saturating_* (each have )    
+                    //libraries to handle the above are wrapping_*, checked_*, overflowing_*, and saturating_* (each have )  
+                //float are always signed f32 or f64 (same speed, 64 more precision)
+    let x = 2.0;
+    let y: f32 = 3.0;   //appended to make 32 bit
+
+    println!("{}", -5.0 / 3.0);
 }
