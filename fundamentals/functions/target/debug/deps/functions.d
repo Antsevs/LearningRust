221c99ddf7d2642ad872e066ead7225e1b768fd2
Anthony Sevarino <sevarinoanthony@gmail.com>
2025-09-03 13:39:05 -0400

    <Learning>(<fundamentals>): Finished functions.
    I finished the functions portion of the fundamentals review. I covered how functions are instantiated and called,
    as well as how to make use of parameters (arguments) for function input and output (calling). Finally, I learned the
    difference between a statement, which is a piece of code that perform actions but do not return a value, and expressions,
    which evaluate a resultant value.

---
+++ fundamentals/functions/target/debug/deps/functions.d
@@ -0,0 +1,5 @@
+C:\Users\sevar\OneDrive\Desktop\Dev\LearningRust\fundamentals\functions\target\debug\deps\functions.d: src\main.rs
+
+C:\Users\sevar\OneDrive\Desktop\Dev\LearningRust\fundamentals\functions\target\debug\deps\functions.exe: src\main.rs
+
+src\main.rs:
