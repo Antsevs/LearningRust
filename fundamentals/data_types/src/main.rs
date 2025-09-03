2431dd22167363dc0ffae06e0557bfc567dc1903
Anthony Sevarino <sevarinoanthony@gmail.com>
2025-09-03 13:04:57 -0400

    <Learning>(<fundamentals>): Finished Data Type.
    The data type portion of the fundamentals review is finished.
    Various data types were reviewed, such as scalar types like integers, floats, booleans, chars,
    as well as compound types like tuples and arrays. In regards to the ladder, both how such memory is stored
    as well as accessing specific elements of the types was discussed.

--- fundamentals/data_types/src/main.rs
+++ fundamentals/data_types/src/main.rs
@@ -21,4 +21,33 @@ fn main(){
     let y: f32 = 3.0;   //appended to make 32 bit
 
     println!("{}", -5.0 / 3.0);
+                //booleans are 1 byte
+                //chars use '' instead of "" for string literals, and is four bytes
+        //Compound types
+            //Tuple (grouping a number of values with variety of types into one compound type)
+                //Once declared cannot shrink or grow in size
+                //define first with type index (annotating), then values
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+                //to grab values of a tuple, pattern match (called destructuring)
+    let(x, y, z) = tup;
+    println!("{}{}{}", x, y, z);
+                //or use dot index
+    let index_one = tup.0;
+    let index_two = tup.1;
+    let index_three = tup.2;
+    println!("The tuple indexes are {index_one}, {index_two}, and {index_three}.");
+                //empty tuples are called a unit
+            //Array (grouping of a number of values with a single type)
+                //fixed length, stored on stack
+    let a = [1, 2, 3, 4, 5];
+            //Vector (non native, require standard library)
+                //dynamically sized, stored on heap
+                //can annotate, but include size of array as well
+    let b: [i32; 5] = [1, 2, 3, 4, 5];
+                //can also fill an array with values
+    let filled = [3; 5];
+                //to access array elements
+    let first = filled[0];
+    let second = filled[1];
+    println!("First is {first} and second is {second}.")
 }
