c78f64a11d22215edfe9c271e06969aad36e6739
Anthony Sevarino <sevarinoanthony@gmail.com>
2025-09-04 20:55:29 -0400

    <Learning>(<Ownership>): Finished 4.2 Referencing and Borrowing
    This chapter discussed the idea of referencing (essentially using pointers), and how it
    behaves like borrowing.

---
+++ ownership/references_and_borrowing/src/main.rs
@@ -0,0 +1,35 @@
+//references can be used to avoid granting owenership of data to function (pasically a ptr to a ptr)
+fn main() {
+    let s1 = String::from("hello");
+
+    let len = calculate_len(&s1);
+
+    println!("The length of '{s1}' is {len}.");
+}
+
+fn calculate_len(s: &String) -> usize {
+    s.len()
+}
+
+//you cannot modify borrowed (referenced) data. error example below:
+fn og() {
+    let s = String::from("hello");
+    change(&s);
+}
+
+fn change(some_string: &s) {
+    some_string.push_str(", world");
+}
+
+//you can fix the above by using a mutable reference
+//the restriction with mutable references is that the data it references cannot have any other references (mut or not) (prevents data races)
+fn og2() {
+    let mut s = String::from("hello");
+    change(&mut s);
+}
+
+fn change2(some_string: &mut s) {
+    some_string.push_str(", world");
+}
+
+//Rust does not allow for dangling references
